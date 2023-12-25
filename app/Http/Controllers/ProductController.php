380c38d78c47ae99dede4e489b826f04fa4948e0
ekaterinaaleinikova <ekaterinaaleinikova61@gmail.com>
2023-12-25 19:17:57 +0600

    first commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,21 +5,43 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
-/*class ProductsController extends Controller
+class ProductsController extends Controller
 {
     public function create(Request $request)
     {
-        $cabinet = new Cabinet();
-        $cabinet->name = "php_cabinet";
-        $cabinet->number = 223;
-        $cabinet->save();
+        $products = new Product();
+        $products->name = $request->name;
+        $products->price = $request->price;  
+        $products->description = $request->description; 
+        $products->stock_quantity = $request->stock_quantity; 
+        $products->category_id = $request->category_id; 
+        $products->save();
         
-        return $cabinet; // Returning the created cabinet
+        return $products;  // Returning the created categories
     }
-}*/ 
-
-/*public function get_cabinets()
+    public function create_list()
     {
-        $cabinets = Cabinet::orderBy('id', 'desc')->get();
-        return $cabinets;
+        $products = Product::orderBy('id', 'desc')->get();
+        return $products;
+    }  
+
+    /*public function update(Request $request, $id)
+    {
+        $categories = Categories::orderBy('id', 'desc')->get();
+        return $categories;
     }*/
+    public function delete($id)
+    {
+        $products = Product::find($id);
+        
+        if ($products) {
+            $products->delete();
+            return "Categories with ID $id has been deleted.";
+        } 
+        else {
+            return "Categories with ID $id was not found.";
+        }
+    }
+}
+
+ 
